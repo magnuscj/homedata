59e84902f1459195eb902a10de40b866fcd02d81
magnuscj <magnus.c.johansson@gmail.com>
2020-12-03 16:24:04 +0100

    just saving

--- visualize/homeAuto_graph.php
+++ visualize/homeAuto_graph.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('Europe/Stockholm');
-require_once ("jpgraph/jpgraph.php");
-require_once ("jpgraph/jpgraph_line.php");
-require_once ('jpgraph/jpgraph_plotline.php');
-require_once ("jpgraph/jpgraph_date.php"); 
-require_once ("jpgraph/jpgraph_regstat.php");
+require_once ("jpgraph.php");
+require_once ("jpgraph_line.php");
+require_once ('jpgraph_plotline.php');
+require_once ("jpgraph_date.php"); 
+require_once ("jpgraph_regstat.php");
 include ("homeFunctions.php");
 
 
@@ -14,14 +14,12 @@ $fileName = $file[sizeof($file)-1].".png";
 
 if(isCli())
 {
-    $path = "pictures\\".$fileName;
-    $path2 = "/var/www/html/pic/".$fileName;
+    $path2 = "/var/www/html/picture/".$fileName;
     $sleepTime = getConfig("SLEEP")+20;
 }
 else
 {
-    $path = "..\\pictures\\".$fileName;
-    $path2 = "J:\\www\\pictures\\".$fileName;
+    $path2 = "/var/www/html/pictue/".$fileName;
     $sleepTime = 60;
 }
 
@@ -34,128 +32,65 @@ do
 		print date('H:i:s',$time).", Working with ".$fileName;
 	}
 	// Create the graph.
-	$graph = new Graph(998,700);
-	$graph->SetScale("datint");
-	$graph->ClearTheme();	
+   $graph = new Graph(998,700);
+   $graph->SetScale("datint");
+   $graph->ClearTheme();	
 
-	$graph->legend->SetLayout(LEGEND_HOR);			// Adjust the legend position
-	$graph->legend->SetFont(FF_VERDANA,FS_NORMAL,8);
-	$graph->legend->SetFillColor('gray:0.43');
-	$graph->legend->SetColumns(2);					// Set number of colums for legend
-	$graph->legend->Pos(0.5,0.05,"center","top");	
-   	$graph->legend->SetColor('gray', 'gray');
-	
-	$graph->xaxis->SetPos("min");
-	$graph->xaxis->title->Set("Datum" ); 
-	$graph->xaxis->scale-> SetDateFormat('d/m H:i'); 
-	$graph->xaxis->SetLabelAngle(80);
-	$graph->xaxis->SetTitleMargin(70);
-    	$graph->xaxis->SetColor('black:1.5','gray'); 
-    	$graph->xaxis->SetFont(FF_VERDANA, FS_NORMAL);
-    	$graph->xgrid->SetColor('black:1.5');
-	$graph->xgrid->Show(true);
-	
-    	$graph->yaxis->title->SetColor('gray');
-    	$graph->yaxis->SetColor('black:1.5','gray'); 
-    	$graph->yaxis->SetFont(FF_VERDANA, FS_NORMAL);
-    	$graph->ygrid->SetColor('black:1.5');
-    
-    	$username		= getConfig("DBUSN");
+   $graph->legend->SetLayout(LEGEND_HOR);			// Adjust the legend position
+   $graph->legend->SetFont(FF_VERDANA,FS_NORMAL,8);
+   $graph->legend->SetFillColor('gray:0.43');
+   $graph->legend->SetColumns(2);					// Set number of colums for legend
+   $graph->legend->Pos(0.5,0.05,"center","top");	
+   $graph->legend->SetColor('gray', 'gray');
+
+   $graph->xaxis->SetPos("min");
+   $graph->xaxis->title->Set("Datum" ); 
+   $graph->xaxis->scale-> SetDateFormat('d/m H:i'); 
+   $graph->xaxis->SetLabelAngle(80);
+   $graph->xaxis->SetTitleMargin(70);
+   $graph->xaxis->SetColor('black:1.5','gray'); 
+   $graph->xaxis->SetFont(FF_VERDANA, FS_NORMAL);
+   $graph->xgrid->SetColor('black:1.5');
+   $graph->xgrid->Show(true);
+
+   $graph->yaxis->title->SetColor('gray');
+   $graph->yaxis->SetColor('black:1.5','gray'); 
+   $graph->yaxis->SetFont(FF_VERDANA, FS_NORMAL);
+   $graph->ygrid->SetColor('black:1.5');
+ 
+  	$username		= getConfig("DBUSN");
 	$password		= getConfig('DBPSW');
 	$database		= getConfig('DBNAME');
 	$serverHostName	        = getConfig('DBIP');	
 	waitDbAlive($serverHostName,$username,$password,$database);
 	$sensors 		= getSensorNames($username,$password,$database, $serverHostName); //From sensor configuration
-	
 	//Index names for the sensor configuration db table
 	$colID			= 0;
-	$colName		= 1;
+	$colName       = 1;
 	$colColor		= 2;
 	$colVisible		= 3;
-	$colType		= 4;
-	$noOfFlowGraphs = 0;
+	$colType       = 4;
+
+	$noOfFlowGraphs    = 0;
 	$noOf_Y_FlowGraphs = 0;
-	$i				= 0;	//General counter/index variable
 		
-	//Determin which sensors that are valid for this view.
-	//The information comes either from the web page or the
-	//db.
-	foreach($sensors[$colName] as $sensorName)
-	{
-		if(isset($_GET[$sensorName]))
-		{
-			$sensorShow[$i] = $_GET[$sensorName];		
-		}
-		else
-		{
-			$isset =false;	
-				
-			if($sensors[$colVisible][$i] == 'True')
-				$sensorShow[$i] 	= "on";
-			else
-				$sensorShow[$i] 	= "off";
-		}
-		$i++;
-	}
-	
 	$ttime = $ftime = date('H:i',time());;
 	
-	if(isset($_GET['fromdate']))
-	{
-		$ftmp  = split(" ", $_GET['fromdate']);
-		$fdate = $ftmp[0];
-		if(sizeof($ftmp)== 2)
-			$ftime = $ftmp[1];
-		
-		$ttmp  = split(" ", $_GET['todate']);
-		$tdate = $ttmp[0]; 
-		if(sizeof($ttmp)== 2)
-			$ttime = $ttmp[1];
-		
-	}
-	else
-	{
-		$fdate = date("Y-m-d", mktime(0,0,0,date("m"),date("d")-1,date("Y")));
-		$tdate = date("Y-m-d", mktime(0,0,0,date("m"),date("d"),date("Y")));
-	}
-	
+	$fdate = date("Y-m-d", mktime(0,0,0,date("m"),date("d")-1,date("Y")));
+	$tdate = date("Y-m-d", mktime(0,0,0,date("m"),date("d"),date("Y")));
 	
 	$graph->title->Set($fdate." ".$ftime." ---> ".$tdate."  ".$ttime);
-    $graph->title->SetColor('gray');
+   $graph->title->SetColor('gray');
 
 	$i=0;
-	
-	$changeConfig = true;
-	
-	if($changeConfig)
-	{
-		foreach($sensors[$colID] as $sensorId)
-		{
-			if($sensorShow[$i] == "on")
-				$show ="True";
-			else
-				$show ="False";
-				
-			$con = mysqli_connect($serverHostName,$username,$password);
-			@mysqli_select_db($con, $database) or die( "Unable to select database");
-			$query = "UPDATE sensorconfig SET visible='".$show. "' WHERE sensorid ='".$sensorId."';";
-			$result = mysqli_query($con, $query);
-			$i++;
-
-			mysqli_close($con);
-		}		
-		$sensors 		= getSensorNames($username,$password,$database,$serverHostName);
-	}
-	
-	$graphArray = array();
 	$senNo = 0;	
 	
 	foreach($sensors[$colID] as $sensorId)
 	{
 		$ydata_temptot = array();
 		$xdata_timeTot = array();
-		
-		if($sensorShow[$senNo] == "on" && $sensors[$colType][$senNo] == "temp")
+		print "\n".$sensors[$colName][$senNo]."  ".$sensors[$colType][$senNo];
+		if( $sensors[$colVisible][$senNo] == "True" && $sensors[$colType][$senNo] == "temp")
 		{
 			$retXY = getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName);		
             /*EXPERIMENT*/
@@ -178,7 +113,7 @@ do
             }
 		}
 		
-		if($sensorShow[$senNo] == "on" && $sensors[$colType][$senNo] == "wind")
+		if( $sensors[$colVisible][$senNo] == "True" && $sensors[$colType][$senNo] == "wind")
 		{
 			$retXY = deltaChange(windAddMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName)))	;
 			$retXY = windMilesTometers($retXY);
@@ -200,7 +135,7 @@ do
 		}
 			
 			
-		if($sensorShow[$senNo] == "on" && $sensors[$colType][$senNo] == "power")
+		if( $sensors[$colVisible][$senNo] == "True" && $sensors[$colType][$senNo] == "power")
 		{
 			$retXY = deltaChange(addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName)));
             $sum = sum($retXY[0], TRUE);
@@ -217,7 +152,7 @@ do
 				$graph->SetYScale($noOf_Y_FlowGraphs,'lin',0,300);
 				$graph->ynaxis[$noOf_Y_FlowGraphs]->SetColor('teal');
 				$graph->ynaxis[$noOf_Y_FlowGraphs]->title->Set('wh');
-                $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetColor('orange');
+            $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetColor('orange');
 				$graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetMargin(11);
 				$graph->ynaxis[$noOf_Y_FlowGraphs]->scale->ticks->Set(20,10); 
 				$graph->ynaxis[$noOf_Y_FlowGraphs]->SetColor('orange');
@@ -237,7 +172,7 @@ do
 			}	
 		}	
 		
-		if($sensorShow[$senNo] == "on" && $sensors[$colType][$senNo] == "rain")
+		if(  $sensors[$colVisible][$senNo] == "True" && $sensors[$colType][$senNo] == "rain")
 		{
 			$retXY = deltaChange(addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName)));
 			//$lineplot2=new LinePlot(scaleChange(0.25 , $retXY[0]),$retXY[1] );
@@ -276,7 +211,6 @@ do
 	{
 			
 		$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
-		//$graph->img->Stream($path);
 		$graph->img->Stream($path2);
 		$utr = time()-$time;
 		print ", it took "."$utr"." seconds. Next run will be in ".$sleepTime." seconds.\n";
@@ -286,7 +220,6 @@ do
 	{
        
         $gdImgHandler = $graph->Stroke(_IMG_HANDLER);
-		//$graph->img->Stream($path);
                 $graph->img->Stream($path2);
 		sleep($sleepTime);
     }
