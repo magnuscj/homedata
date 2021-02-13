c17da9a50a1abb632a1b8191fa6934b655a42454
magnuscj <magnus.c.johansson@gmail.com>
2021-02-13 14:37:13 +0100

    Clean up code

--- visualize/homeAutoGraphMob4.php
+++ visualize/homeAutoGraphMob4.php
@@ -25,24 +25,9 @@ do
    $graph = new Graph(395,219);
    $graph->SetScale("datint");
    $graph->ClearTheme();
-   $graph->xaxis->SetPos("min");
-   $graph->SetMargin(40,42,10,25);
-   $graph->xgrid->Show(true);
-   $graph->xaxis->scale-> SetDateFormat('H'); 
-   $graph->xaxis->SetLabelAngle(0);
-   $graph->yaxis->SetTitleMargin(29);
-   $graph->yaxis->title->Set('°C');
-   $graph->yaxis->title->SetColor('gray');
-   $graph->yaxis->title->SetFont(FF_VERDANA, FS_BOLD,8);
-   $graph->yaxis->SetColor('black:1.5','gray'); 
-   $graph->xaxis->SetColor('black:1.5','gray'); 
-   $graph->xaxis->SetTitleMargin(1);
-   $graph->xaxis->SetFont(FF_VERDANA, FS_BOLD,8);	
-   $graph->yaxis->SetFont(FF_VERDANA, FS_BOLD,8);
-   $graph->xgrid->SetColor('black:1.5');
-   $graph->ygrid->SetColor('black:1.5');
    $graph->SetColor('gray:0.43');
    $graph->SetBackgroundGradient('black:1.1','black:1.1',GRAD_HOR,BGRAD_MARGIN);
+   $graph->SetMargin(40,42,10,25);
 	
    $color         = array("black", "blue","red","green","brown");
 	$configuration = array(array("orange",           /*Line color*/
@@ -55,10 +40,7 @@ do
                             "aquamarine3",      /*Y-axix title color*/
                             "aquamarine3",      /*Y-axix color */
                             "xxxx"));
-    
-	$max		   = array("-",  "-","-","-","-");
-	$min		   = array("-",  "-","-","-","-");
-	$curr		   = array("-",  "-","-","-","-");
+	
 	$username	   = getConfig("DBUSN");
 	$password	   = getConfig('DBPSW');
 	$database	   = getConfig('DBNAME');
@@ -68,14 +50,6 @@ do
 
 	$sensors 	   = getSensorNames($username,$password,$database,$serverHostName);
 	
-	$i=0;
-	foreach($sensors[1] as $sensorName) //make all sensors invisible but one
-	{
-	   $sensorShow[$i] 	= "off";	
-		if(($sensorName == "Ute" ) || ($sensorName == "El"))
-			$sensorShow[$i] = "on"; //Make this one visible
-		$i++;
-	}
 	$fdate 		= date("Y-m-d", mktime(0,0,0,date("m"),date("d")-1,date("Y")));
 	$tdate 		= date("Y-m-d", mktime(0,0,0,date("m")+0,date("d"),date("Y")));		
 	$Nowtime 	= date('H:i:s',time());
@@ -83,96 +57,70 @@ do
 	$fsplited 	= preg_split ( '/-/' ,$fdate  );
 	$tsplited 	= preg_split ( '/-/' ,$tdate  );
 	
-	$frommonth = (int)$fsplited[1];
-	$tomonth   = (int)$tsplited[1];
-	$fromyear  = (int)$fsplited[0];
+	$frommonth  = (int)$fsplited[1];
+	$tomonth    = (int)$tsplited[1];
+	$fromyear   = (int)$fsplited[0];
 	$toyear	   = (int)$tsplited[0];
 	$ttime = $ftime = date('H:i',time());
-	$i = 0;	
-    
+	
+     
+   $graph->xgrid->Show(true);
+   $graph->xaxis->scale-> SetDateFormat('H');
+   $graph->xaxis->SetColor('black:1.5','gray');   
+   $graph->xaxis->SetFont(FF_VERDANA, FS_BOLD,8);
+
+   /*------------- El  -------------*/
+   $graph->yaxis->SetTitleMargin(28);
+   $graph->yaxis->SetFont(FF_VERDANA, FS_BOLD,8);
+   $graph->yaxis->SetColor($configuration[1][3]); 
+   $graph->yaxis->SetTitleSide(SIDE_LEFT);
+   $graph->yaxis->title->SetFont(FF_VERDANA, FS_BOLD,8);
+   $graph->yaxis->title->Set($configuration[1][1]);
+   $graph->yaxis->title->SetColor($configuration[1][2]);
+   $graph->yaxis->title->SetMargin(0);
+   $graph->yaxis->scale->ticks->Set(20,10); 
+   
+   /*------------- Ute -------------*/
+   $noOf_Y_FlowGraphs = 0;
+   $graph->SetYScale($noOf_Y_FlowGraphs,'lin');
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetTitleMargin(32);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetFont(FF_VERDANA, FS_BOLD, 8);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetColor($configuration[0][3]);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetTitleSide(SIDE_RIGHT);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetFont(FF_VERDANA, FS_BOLD,8);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->title->Set($configuration[0][1]);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetColor($configuration[0][2]);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetMargin(4);
+   $graph->ynaxis[$noOf_Y_FlowGraphs]->scale->ticks->Set(20,10); 
+
+   $colName       = 1;
+   $i = 0;
 	foreach($sensors[0] as $sensorId)
 	{
-		if(($sensorShow[$i] == "on") && ($i==3))
+		if($sensors[$colName][$i]=="El")
 		{
-	      $ydata_temptot = array();
-			$xdata_timeTot = array();
-         if($sensors[1][$i]=="El")
-            $retXY = deltaChange(addMissingTime(getDataFromDb($username, $password, 
-			           $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName)));
-         else
-			  $retXY = getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." "
-			           .$ttime, $sensorId, $serverHostName);
-
-			$ydata_temptot = $retXY[0];
-			$xdata_timeTot = $retXY[1];
-				
-			if(($ydata_temptot != null && $xdata_timeTot) && $xdata_timeTot[0])
-			{
-            $confNo = 1;
-				$ydata_temptot = floatAvg(10, $ydata_temptot);
-				$lineplot = new LinePlot($ydata_temptot, $xdata_timeTot);              
-				$lineplot->SetColor($configuration[$confNo][0]);
-				$lineplot->SetWeight(2);
-            if($sensors[1][$i]=="El")
-            {
-              $lineplot->SetFillGradient('aquamarine1','black:1.1');
-            }
-				$graph->Add($lineplot);
-            $graph->yaxis->title->Set($configuration[$confNo][1]);
-            $graph->yaxis->title->SetColor($configuration[$confNo][2]);
-				$graph->yaxis->title->SetMargin(0);
-            $graph->yaxis->scale->ticks->Set(20,10); 
-            $graph->yaxis->SetColor($configuration[$confNo][3]); 
-            $graph->yaxis->SetTitleSide(SIDE_LEFT);              
-			}
+         $confNo = 1;
+         $retXY = deltaChange(addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName)));
+         $lineplot = new LinePlot(floatAvg(10, $retXY[0]), $retXY[1]);              
+         $lineplot->SetColor($configuration[$confNo][0]);
+         $lineplot->SetWeight(2);
+         $lineplot->SetFillGradient('aquamarine1','black:1.1');
 		}
       
-		if(($sensorShow[$i] == "on") && ($i==0))
+		if($sensors[$colName][$i]=="Ute")
 		{
          $confNo = 0;
-         if($sensors[1][$i]=="El")
-         {
-            $retXY = deltaChange(addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName)));
-         }
-         else
-         {
-            $retXY = getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName);
-         }
-
+         $retXY = getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName);
 		   $lineplot2=new LinePlot(floatAvg(5, $retXY[0]),$retXY[1] );
 		   $lineplot2->SetColor($configuration[$confNo][0]);
          $lineplot2->SetWeight(2);
-
-         if($sensors[1][$i]=="El")
-         {
-           $lineplot2->SetFillGradient('aquamarine1','black:1.1');		
-         }
-            
-         $noOf_Y_FlowGraphs = 0;
-         if($sensors[1][$i]=="El")
-         {
-           $graph->SetYScale($noOf_Y_FlowGraphs,'lin',0,220);
-         }
-         else
-         {
-           $graph->SetYScale($noOf_Y_FlowGraphs,'lin');
-         }
-		    
-         $graph->ynaxis[$noOf_Y_FlowGraphs]->title->Set($configuration[$confNo][1]);
-         $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetColor($configuration[$confNo][2]);
-         $graph->ynaxis[$noOf_Y_FlowGraphs]->SetFont(FF_VERDANA, FS_BOLD, 8);
-         $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetFont(FF_VERDANA, FS_BOLD,8);
-         $graph->ynaxis[$noOf_Y_FlowGraphs]->SetTitleMargin(36);
-		   $graph->ynaxis[$noOf_Y_FlowGraphs]->title->SetMargin(4);
-		   $graph->ynaxis[$noOf_Y_FlowGraphs]->scale->ticks->Set(20,10); 
-		   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetColor($configuration[$confNo][3]);
-		   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetPos('max');				
-		   $graph->ynaxis[$noOf_Y_FlowGraphs]->SetTitleSide(SIDE_RIGHT);
-		   $graph->AddY($noOf_Y_FlowGraphs,$lineplot2);
 		}	
 		$i++;
 	}
-	
+   
+   $graph->Add($lineplot);
+   $graph->AddY($noOf_Y_FlowGraphs,$lineplot2);
+
 	if(isCli())
 	{
 		$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
@@ -180,15 +128,13 @@ do
 	
 		$utr = time()-$time;
 		print ", "."$utr"."s, sleep ".$sleepTime."s\n";
+      exit(1);
 		sleep($sleepTime);
 	}
 	
 	if(!isCli())
 	{
-		// Display the graph
-		
 		$graph->Stroke();
-		
 	}
 }while (isCli());
 ?>
