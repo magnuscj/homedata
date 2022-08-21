d5142588f208bd4f2bf98eaffcd0b79d30bc96c9
magnuscj <magnus.c.johansson@gmail.com>
2022-08-21 07:31:14 +0200

    Add freezer and fridge supervision by Philips sensors

---
+++ scripts/hueTemps.py
@@ -0,0 +1,57 @@
+import subprocess
+import time
+from datetime import datetime
+
+encoding = 'utf-8'
+i=1
+tempSamples = []
+tempSamplesSort = []
+tempSamplesMax = []
+tempDirSamples = []
+maxTempSamples = 60*5
+maxTempMaxSamples = 60*60
+pollCount = 0
+errors = 0
+success = 0
+
+
+
+now = datetime.now()
+timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+macdata = subprocess.Popen("ip addr show | grep -m1 ether | awk {'print $2'}", shell=True, stdout=subprocess.PIPE)
+mac = macdata.stdout.read()
+id1 = mac.rstrip()+":1"
+
+while 1:
+    pollCount+=1    
+    start_time = time.time()
+    temperatur = subprocess.Popen('curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"14\\".state.temperature', shell=True, stdout=subprocess.PIPE)
+    temp = str(float(temperatur.stdout.read())/100)
+    try:
+        tempSamples.append(temp)
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(r'tmpl_Huedetails.xml', 'r') as file:
+            data = file.read()
+            file.close()
+            data = data.replace("#TIME#", timestamp)
+            data = data.replace("#TEMP#", temp)
+            data = data.replace("#LOOPTIME#", str(time.time() - start_time))
+            data = data.replace("#MAC#", mac)
+            data = data.replace("#ID1#", id1.rstrip())
+            data = data.replace("#POLLCOUNT#", str(pollCount))
+            data = data.replace("#ERRORS#", str(errors))
+
+        with open(r'/mnt/ramdisk/details.xml', 'w') as file:
+            file.write(data) 
+            file.close()
+        success+=1
+        print(temp)
+        time.sleep(600)
+    except Exception as e:
+        errors+=1
+        est = timestamp + " " + str(e)
+        print(est)
+    i=0
