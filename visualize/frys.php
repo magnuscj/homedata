4f50123fca70b84108facd3ffeb2dcea2090241e
magnuscj <magnus.c.johansson@gmail.com>
2022-10-02 09:51:37 +0200

    Add simple status view

---
+++ visualize/frys.php
@@ -0,0 +1,123 @@
+<?php
+date_default_timezone_set('Europe/Stockholm');
+require_once ("jpgraph.php");
+require_once ("jpgraph_line.php");
+include ("jpgraph_date.php"); 
+include ("jpgraph_regstat.php");
+include ("homeFunctions.php");
+
+$file = explode('.', __FILE__);
+$file = explode('/', $file[0]);
+$fileName = $file[sizeof($file)-1].".png";
+$path = getConfig("PATH");
+
+$sleepTime = getConfig("SLEEP")+30;
+
+do
+{
+	if(isCli())
+   {
+      $time = time();
+      print date('H:i:s',$time).", ".$fileName;
+   }
+
+   // Create the graph.
+   $graph = new Graph(296,109);
+   $graph->SetScale("datlin",-25,0);
+   $graph->yaxis->scale->ticks->Set(10,2);
+   
+   $graph->ClearTheme();
+   $graph->SetColor('gray:0.43');
+   $graph->SetBackgroundGradient('black:1.1','black:1.1',GRAD_HOR,BGRAD_MARGIN);
+   $graph->SetMargin(40,22,10,25);
+	
+   $color         = array("black", "blue","red","green","brown");
+	$configuration = array(array("orange",           /*Line color*/
+                                    "°C",           /*Y-axis name*/
+                                "orange",           /*Y-axix title color*/
+                                "orange",           /*Y-axix color */
+                                 "xxxx"),
+                           array("cornflowerblue:1.2",/*Line color*/
+                            "grader",               /*Y-axis name*/
+                            "cornflowerblue",      /*Y-axix title color*/
+                            "cornflowerblue",      /*Y-axix color */
+                            "xxxx"));
+	
+	$username	   = getConfig("DBUSN");
+	$password	   = getConfig('DBPSW');
+	$database	   = getConfig('DBNAME');
+	$serverHostName= getConfig('DBIP');
+   
+	waitDbAlive($serverHostName,$username,$password,$database);
+
+	$sensors 	= getSensorNames($username,$password,$database,$serverHostName);
+	$fdate 		= date("Y-m-d", mktime(0,0,0,date("m"),date("d")-1,date("Y")));
+	$tdate 		= date("Y-m-d", mktime(0,0,0,date("m"),date("d"),date("Y")));		
+	$Nowtime 	= date('H:i:s',time());
+	$fsplited 	= preg_split ( '/-/' ,$fdate  );
+	$tsplited 	= preg_split ( '/-/' ,$tdate  );
+	$frommonth  = (int)$fsplited[1];
+	$tomonth    = (int)$tsplited[1];
+	$fromyear   = (int)$fsplited[0];
+	$toyear	   = (int)$tsplited[0];
+	$ttime = $ftime = date('H:i',time());
+
+   //Graph time stamp
+   $length     = 104.0;
+	$t2 = new Text($tdate.", ".date("H:i"),$length*2.94,209);
+	$t2->SetFont(FF_ARIAL,FS_NORMAL,8);
+	$t2->SetColor('gray:0.63');
+   $t2->ParagraphAlign('right');
+	$graph->AddText($t2);
+	
+   $graph->xgrid->Show(true);
+   $graph->xaxis->scale-> SetDateFormat('H');
+   $graph->xaxis->SetColor('black:1.5','gray');   
+   $graph->xaxis->SetFont(FF_VERDANA, FS_BOLD,8);
+
+   /*------------- El  -------------*/
+   $graph->yaxis->SetTitleMargin(18);
+   $graph->yaxis->SetFont(FF_VERDANA, FS_BOLD,8);
+   $graph->yaxis->SetColor($configuration[1][3]); 
+   $graph->yaxis->SetTitleSide(SIDE_LEFT);
+   $graph->yaxis->title->SetFont(FF_VERDANA, FS_BOLD,8);
+   $graph->yaxis->title->Set($configuration[1][1]);
+   $graph->yaxis->title->SetColor($configuration[1][2]);
+   $graph->yaxis->title->SetMargin(0);
+   
+   $colName       = 1;
+   $i = 0;
+	foreach($sensors[0] as $sensorId)
+	{
+		if($sensors[$colName][$i]=="Fry_ko")
+		{
+         $confNo = 1;
+         $retXY = addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, $sensorId, $serverHostName));
+         $lineplot = new LinePlot(floatAvg(10, $retXY[0]), $retXY[1]);              
+         $lineplot->SetColor($configuration[$confNo][0]);
+         $lineplot->SetWeight(2);
+         $lineplot->SetFillGradient('cornflowerblue','black:1.1');
+		}
+      
+		$i++;
+	}
+   
+   $graph->Add($lineplot);
+ 
+	if(isCli())
+	{
+		$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+		$graph->img->Stream($path.$fileName);
+	
+		$utr = time()-$time;
+		print ", "."$utr"."s, sleep ".$sleepTime."s\n";
+      exit(1);
+		sleep($sleepTime);
+	}
+	
+	if(!isCli())
+	{
+		$graph->Stroke();
+	}
+}while (isCli());
+?>
---
+++ visualize/status.php
@@ -0,0 +1,154 @@
+<?php
+date_default_timezone_set('Europe/Stockholm');
+require_once ("jpgraph.php");
+require_once ("jpgraph_line.php");
+require_once ('jpgraph_plotline.php');
+require_once ("jpgraph_date.php");
+require_once ("jpgraph_regstat.php");
+require_once ("jpgraph_bar.php");
+require_once ('jpgraph_canvas.php');
+include ("homeFunctions.php");
+
+
+$file = explode(".php", __FILE__);
+$file = explode("/", $file[0]);
+$fileName = $file[sizeof($file)-1].".png";
+
+if(isCli())
+{
+    $path = "pictures\\".$fileName;
+    $path2 = "/var/www/html/picture/".$fileName;
+	 $sleepTime = getConfig("SLEEP")+20;
+}
+else
+{
+    $path = "..\\pictures\\".$fileName;
+    $path2 = "J:\\www\\pictures\\".$fileName;
+    $sleepTime = 60;
+}
+
+$resources = array("Fry_ko", "Kyl_ko", "Kyl_gr", "kylFrys", "Sovrum", "Garage", "Skorst");
+
+do 
+{
+	if(isCli())
+	{
+		$time = time();
+		print date('H:i:s',$time).", ".$fileName;
+	}
+	$debug          = false;
+	$username		= getConfig("DBUSN");
+	$password		= getConfig('DBPSW');
+	$database		= getConfig('DBNAME');
+	$serverHostName	= getConfig('DBIP');
+    waitDbAlive($serverHostName,$username,$password,$database);
+    $textColor      = 'gray:2.7';
+    $frameColor     = 'gray:0.43';
+    $backGroundClr  = 'gray:0.43';
+   	$sensors 		= getSensorNames($username,$password,$database, $serverHostName); //From sensor configuration
+	
+    $sensorNameToShow = "Fry_ko";
+    $length     = 57.5;
+    $canLength  = $length*3.8/2.0*1.0;
+    $canHight   = $length*1.64*1.2;
+    
+	$graph = new CanvasGraph($canLength,$canLength-5,'auto');
+	$graph->SetMargin(7,7,7,7);
+	$graph->SetMarginColor($frameColor);
+	$graph->SetColor($backGroundClr);
+    $graph->InitFrame();
+   
+    $Y=20;
+    foreach ($resources as $resource)
+    {
+        $sensorId = getSensorId($resource, $username,$password,$database,$serverHostName);
+        $temp = number_format(getCurr($sensorId,$username,$password,$serverHostName,$database),1);
+        $tempValue = floatval($temp);
+        $skip=0;
+        $skift=0;
+
+        if ((explode('_', $resource)[0] == "Fry") || $resource=="kylFrys")
+        {
+            $min=-25.0;
+            $max=-10.0;
+        }
+        elseif (explode('_', $resource)[0] == "Kyl")
+        {
+            $min=4.0;
+            $max=7.0;
+        }
+        elseif ($resource == "XSovrum")
+        {
+            $min=17.0;
+            $max=21.0;
+        }
+        elseif ($resource == "Garage")
+        {
+            $min=2.0;
+            $max=15.0;
+            $skift=1;
+        }
+        elseif ($resource == "Skorst")
+        {
+            $min=18.0;
+            $max=65.0;
+        }
+        else
+        {
+            $skip=1;
+        }
+
+        if($skip==0)
+        {
+            $tempValue = $tempValue < $min ? $min : $tempValue;
+            $tempValue = $tempValue > $max ? $max : $tempValue;
+
+            $f = ($tempValue -$min)/($max-$min);
+            $R = !$skift ? 255*$f : 255*(1-$f);
+            $G = $skift ? 255*$f : 255*(1-$f);
+            $B=0;
+
+            $color = array($R, $G, $B);
+            $resource = $resource=="kylFrys" ? "Fry_gr" : $resource;
+            $t = new Text($resource);       
+            $t->SetPos(10,$Y,'left');
+            $t->SetFont(FF_ARIAL,FS_BOLD,$length*0.155);
+            $t->SetColor($textColor);
+            $t->Align('left','bottom');
+            $graph->AddText($t);
+
+            $te = new Text($temp);       
+            $te->SetPos(80,$Y,'right');
+            $te->SetFont(FF_ARIAL,FS_BOLD,$length*0.155);
+            $te->SetColor($textColor);
+            $te->Align('right','bottom');
+            $graph->AddText($te);
+
+            $graph->img->SetColor($color);
+            $graph->img->FilledCircle($canLength - 15, -5 + $Y,4);
+
+            $Y = $Y + 12;
+        }
+
+    }
+    
+   	if(isCli())
+	{
+		$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+        $graph->img->Stream($path2);		
+		$utr = time()-$time;
+		print ", "."$utr"."s sleep "."$sleepTime"."s\n";
+        print $path2;
+        exit(1);
+		sleep($sleepTime);
+	}
+    else
+    {
+        $gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+        $graph->img->Stream($path2);
+		sleep($sleepTime);
+    }
+		
+}while (true);
+	
+?>
