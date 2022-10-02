88f6ae9b7465deca76da07b86884fca4132972b6
magnuscj <magnus.c.johansson@gmail.com>
2022-10-02 14:00:26 +0200

    Improve Hue support

--- scripts/hueTemps.py
+++ scripts/hueTemps.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 encoding = 'utf-8'
 i=1
 tempSamples = []
+sensorTimeSamples = []
+battSamples = []
 tempSamplesSort = []
 tempSamplesMax = []
 tempDirSamples = []
@@ -24,35 +26,37 @@ id1 = mac.strip()+":1"
 id2 = mac.strip()+":2"
 id3 = mac.strip()+":3"
 
+strx1 ='curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"'
+strx2 =['14','33','36']
+strx3 ='\\".state.temperature'
+sLastTime ='\\".state.lastupdated'
+sBatt ='\\".config.battery'
+
+
 while 1:
     pollCount+=1    
     start_time = time.time()
     del tempSamples[:]
-    temperatur = subprocess.Popen('curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"14\\".state.temperature', shell=True, stdout=subprocess.PIPE)
-    temp = str(float(temperatur.stdout.read())/100)
-    tempSamples.append(temp)
+    for n in strx2:
+        strx = strx1 + n + strx3
+        temperatur = subprocess.Popen(strx, shell=True, stdout=subprocess.PIPE)
+        temp = str(float(temperatur.stdout.read())/100)
+        tempSamples.append(temp)
 
-    temperatur = subprocess.Popen('curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"33\\".state.temperature', shell=True, stdout=subprocess.PIPE)
-    temp = str(float(temperatur.stdout.read())/100)
-    tempSamples.append(temp)
-
-    temperatur = subprocess.Popen('curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"36\\".state.temperature', shell=True, stdout=subprocess.PIPE)
-    temp = str(float(temperatur.stdout.read())/100)
-    tempSamples.append(temp)
-
-
-    sensorTime = subprocess.Popen('curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"14\\".state.lastupdated', shell=True, stdout=subprocess.PIPE)
-
-    battery = subprocess.Popen('curl -sX GET http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"14\\".config.battery', shell=True, stdout=subprocess.PIPE)
-
-    batt = str(float(battery.stdout.read()))
-    try:
+        strx = strx1 + n + sLastTime
+        sensorTime = subprocess.Popen(strx, shell=True, stdout=subprocess.PIPE)
         now = datetime.now()
         timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
         sTime = sensorTime.stdout.read().decode("utf-8").replace('"\n','').replace('"','')
         delta = now - datetime.strptime(sTime, "%Y-%m-%dT%H:%M:%S") - timedelta(hours=2)
+        sensorTimeSamples.append(delta)
 
+        strx = strx1 + n + sBatt
+        battery = subprocess.Popen(strx, shell=True, stdout=subprocess.PIPE)
+        batt = str(float(battery.stdout.read()))
+        battSamples.append(batt)
 
+    try:
         with open(r'tmpl_Huedetails.xml', 'r') as file:
             data = file.read()
             file.close()
@@ -62,8 +66,12 @@ while 1:
             data = data.replace("#TEMP3#", tempSamples[2])
             data = data.replace("#LOOPTIME#", str(time.time() - start_time))
             data = data.replace("#MAC#", mac)
-            data = data.replace("#BATT1#", batt)
-            data = data.replace("#UPDATE1#", str(delta).split('.')[0])
+            data = data.replace("#BATT1#", battSamples[0])
+            data = data.replace("#BATT2#", battSamples[1])
+            data = data.replace("#BATT3#", battSamples[2])
+            data = data.replace("#UPDATE1#", str(sensorTimeSamples[0]).split('.')[0])
+            data = data.replace("#UPDATE2#", str(sensorTimeSamples[1]).split('.')[0])
+            data = data.replace("#UPDATE3#", str(sensorTimeSamples[2]).split('.')[0])
             data = data.replace("#ID1#",id1.rstrip())
             data = data.replace("#ID2#",id2.rstrip())
             data = data.replace("#ID3#",id3.rstrip())
@@ -73,8 +81,9 @@ while 1:
         with open(r'/mnt/ramdisk/details.xml', 'w') as file:
             file.write(data) 
             file.close()
-        success+=1
-        time.sleep(600)
+            success+=1
+            time.sleep(600)
+
     except Exception as e:
         errors+=1
         est = timestamp + " " + str(e)
