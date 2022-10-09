0d4429a927363f5412d3705a53593752d058ea3b
magnuscj <magnus.c.johansson@gmail.com>
2022-10-09 07:18:30 +0200

    Add template files

--- scripts/updateip.sh
+++ scripts/updateip.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
+
+#Retreive external IP
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-sed "s/#IP#/$ip/g" "tmpl_$1" > $1
+
+echo "Current ip  : $EXTERNAL_IP"
+echo "Retreived ip: $ip"
+
+if [[ $ip != $EXTERNAL_IP ]]; then
+  if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    export EXTERNAL_IP=$ip
+    echo "Current ip  : $EXTERNAL_IP"
+    if [[ -z $1 ]]; then
+      echo "No template provided"
+    else
+      #Create file with new ip
+      sed "s/#IP#/$ip/g" "tmpl_$1" > $1
+    fi
+  else
+    echo "fail"
+  fi
+fi
