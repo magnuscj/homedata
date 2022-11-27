13d9500f4c26632d952570594ba57a86c2c5e620
magnuscj <magnus.c.johansson@gmail.com>
2022-11-27 13:06:22 +0100

    Update cron

--- container/build.sh
+++ container/build.sh
@@ -1,5 +1,5 @@
 echo $1
-docker image build -t magnuscj/eds:$1 .;docker push magnuscj/eds:$1
+docker image build --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t magnuscj/eds:$1 .;docker push magnuscj/eds:$1
 cp eds.yaml eds_deploy.yaml
 sed -i "s/REPLACE/$1/g" eds_deploy.yaml
 kubectl apply -f eds_deploy.yaml
