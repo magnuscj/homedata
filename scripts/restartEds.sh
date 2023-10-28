493629b2d0c729b003fb2857dcec62b5e44a703a
magnuscj <magnus.c.johansson@gmail.com>
2023-10-28 09:01:44 +0200

    Add simple restart of a EDS that lost network

---
+++ scripts/restartEds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Started "$(date)
+while [ 1 ]
+do
+  REACT=$(ping 192.168.1.245 -c 1 | grep Unreachable | awk '{print "false"}')
+
+  if [[ $REACT == "false" ]];
+  then
+    echo "Restarted "$(date)
+    curl --request PUT --data '{"on":false}' http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/lights/18/state
+    echo " "
+    sleep 50
+    curl --request PUT --data '{"on":true}' http://192.168.1.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/lights/18/state
+    echo " "
+    sleep 3600
+  fi
+  sleep 800
+done
+echo "Done!"
