56a17867ca41f6d40387a5d38e944e005354e75b
magnuscj <magnus.c.johansson@gmail.com>
2023-11-25 15:35:42 +0100

    Correction

--- container/potpod/humidity.py
+++ container/potpod/humidity.py
@@ -25,17 +25,11 @@ logger.addHandler(ch)
 
 encoding = 'utf-8'
 i=1
-tempSamples = []
+
 humiSamples = []
 data = []
-sensorTimeSamples = []
-battSamples = []
-tempSamplesSort = []
-tempSamplesMax = []
-tempDirSamples = []
+
 ids = []
-maxTempSamples = 60*5
-maxTempMaxSamples = 60*60
 pollCount = 0
 errors = 0
 success = 0
@@ -47,7 +41,7 @@ timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
 macdata = subprocess.Popen("ip addr show | grep -m1 ether | awk {'print $2'}", shell=True, stdout=subprocess.PIPE)
 mac = macdata.stdout.read().decode("utf-8")
 
-for x in range(5,13):
+for x in range(1,13):
     ids.append(mac.strip()+":"+str(x))
 logger.debug(ids)
 
@@ -58,15 +52,6 @@ while 1:
     start_time = time.time()
     
     del humiSamples[:]
-    del sensorTimeSamples[:]
-    del battSamples[:]
-       
-    logger.debug("Temperature samples")
-    logger.debug(tempSamples)
-    logger.debug("Battery samples")
-    logger.debug(battSamples)
-    logger.debug("Delta time samples")
-    logger.debug(sensorTimeSamples)
     
     try:
         response = urlopen(url)
@@ -89,22 +74,14 @@ while 1:
         with open(r'tmpl_humidetails.xml', 'r') as file:
             data = file.read()
             file.close()
-            data = data.replace("#TIME#", timestamp)
             
             for i in range(len(humiSamples)):
                 data = data.replace("#HUMI"+str(i)+"#", humiSamples[i])
 
             data = data.replace("#LOOPTIME#", str(time.time() - start_time))
             data = data.replace("#MAC#", mac)
-            for i in range(len(battSamples)):
-                data = data.replace("#BATT"+str(i)+"#", battSamples[i])
             
-            data = data.replace("#UPDATE1#", sensorTimeSamples[0])
-            data = data.replace("#UPDATE2#", sensorTimeSamples[1])
-            data = data.replace("#UPDATE3#", sensorTimeSamples[2])
-            data = data.replace("#UPDATE4#", sensorTimeSamples[3])
-
-            for i in range(0,len(ids)):
+            for i in range(5,len(ids)):
                 data = data.replace("#ID"+str(i+1)+"#",ids[i].rstrip())
 
             data = data.replace("#POLLCOUNT#", str(pollCount))
