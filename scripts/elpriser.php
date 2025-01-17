def297425d0e432537baa8199d09077ddd1541a4
magnuscj <magnus.c.johansson@gmail.com>
2025-01-17 16:44:57 +0100

    Fix code

--- scripts/elpriser.php
+++ scripts/elpriser.php
@@ -8,44 +8,38 @@ include ("jpgraph_regstat.php");
 $fileName = "prices_".date("Y_m_d", mktime(0,0,0,date("m"),date("d")+1,date("Y")));
 $datay = file($fileName.".txt");
 
-$i = 1;
-do
-{
-   $time = time();
-   print date('H:i:s',$time).", ".$fileName;
+
+$time = time();
+print date('H:i:s',$time).", ".$fileName;
    
 
-   // Create the graph.
-   $graph = new Graph(296,109);
-   $graph->SetScale("intlin",0,2);
-   
-   $graph->ClearTheme();
+// Create the graph.
+$graph = new Graph(296,109);
+$graph->SetScale("intlin",0,2);
+$graph->ClearTheme();
+$graph->SetBox(false);
+$graph->SetColor('gray:0.43');
+$graph->SetBackgroundGradient('black:1.1','black:1.1',GRAD_HOR,BGRAD_MARGIN);
+$graph->SetMargin(40,22,10,25);
 
-   $graph->SetBox(false);
-   $graph->SetColor('gray:0.43');
-   $graph->SetBackgroundGradient('black:1.1','black:1.1',GRAD_HOR,BGRAD_MARGIN);
-   $graph->SetMargin(40,22,10,25);
-	
-   $graph->xgrid->Show(true);
-   $graph->xaxis->SetColor('black:1.5','gray');   
-   
-   $graph->yaxis->SetTitleMargin(18);
-   $graph->yaxis->SetColor('gray'); 
-   $graph->yaxis->SetTitleSide(SIDE_LEFT);
-   $graph->yaxis->title->SetColor('red');
-   $graph->yaxis->title->SetMargin(0);
-   
-     // Create the line
-   $p1 = new LinePlot(array_map('floatval',$datay));
-   $p1->SetColor('yellow');
-   $p1->SetStepStyle();
-   $graph->Add($p1);
+// Configure the x-axis
+$graph->xgrid->Show(true);
+$graph->xaxis->SetColor('black:1.5','gray');   
 
-   $gdImgHandler = $graph->Stroke(_IMG_HANDLER);
-   $graph->img->Stream($fileName.".png");
-   $utr = time()-$time;
+// Configure the y-axis
+$graph->yaxis->SetTitleMargin(18);
+$graph->yaxis->SetColor('gray'); 
+$graph->yaxis->SetTitleSide(SIDE_LEFT);
+$graph->yaxis->title->SetColor('red');
+$graph->yaxis->title->SetMargin(0);
    
-   $i = 0;
 
-}while ($i);
+$p1 = new LinePlot(array_map('floatval',$datay));
+$p1->SetColor('yellow');
+$p1->SetStepStyle();
+$graph->Add($p1);
+
+$gdImgHandler = $graph->Stroke(_IMG_HANDLER);
+$graph->img->Stream($fileName.".png");
+   
 ?>
