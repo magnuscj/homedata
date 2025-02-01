ce3eb810e181a49e382ca64e54f8bb791d23080c
magnuscj <magnus.c.johansson@gmail.com>
2025-02-01 15:48:49 +0100

    Updates

--- container/huepod/huesensors.py
+++ container/huepod/huesensors.py
@@ -1,10 +1,20 @@
+from asyncio.windows_events import NULL
 import subprocess
 import time
+from time import sleep  # Import sleep function
 import json
 import logging
+import requests
 from datetime import datetime, timedelta
 from urllib.request import urlopen
 from itertools import islice
+from tabulate import tabulate
+import os  # Import os module
+
+PATH = '/container/huepod/'
+TEMPLATE_ITEM_FILE = 'detail.xml'
+TEMPLATE_FILE = 'detailes.xml'
+OUTPUT_FILE = '/mnt/ramdisk/detailes.xml'
 
 # Create a logger
 logger = logging.getLogger('logger')
@@ -23,74 +33,160 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
-encoding = 'utf-8'
-i=1
-tempSamples = []
-humiSamples = []
-data = []
-sensorTimeSamples = []
-battSamples = []
-tempSamplesSort = []
-tempSamplesMax = []
-tempDirSamples = []
-ids = []
-maxTempSamples = 60*5
-maxTempMaxSamples = 60*60
-pollCount = 0
-errors = 0
-success = 0
-
-
-
 now = datetime.now()
 timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-macdata = subprocess.Popen("ip addr show | grep -m1 ether | awk {'print $2'}", shell=True, stdout=subprocess.PIPE)
-mac = "1c:69:7a:02:8c:4c"  #macdata.stdout.read().decode("utf-8")
+start_time = time.time()
 
-for x in range(1,14):
-    ids.append(mac.strip()+":"+str(x))
-logger.debug(ids)
-
-url = "http://192.168.50.237/get_livedata_info?"
-
-urlHue ='curl -sX GET http://192.168.50.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/sensors | jq .\\"'
-hueSensors =['14','33','36','80','83']
-urlTemperature ='\\".state.temperature'
-sLastTime ='\\".state.lastupdated'
-sBatt ='\\".config.battery'
-
-while 1:
-    pollCount+=1    
-    start_time = time.time()
-    del tempSamples[:]
-    del humiSamples[:]
-    del sensorTimeSamples[:]
-    del battSamples[:]
-       
-    for sensor in range(1,5):
-        getUrlData = urlHue + sensor + urlTemperature
-        print(getUrlData)
-        temperatur = subprocess.Popen(getUrlData, shell=True, stdout=subprocess.PIPE)
-        
-        try:
-            temp = str(float(temperatur.stdout.read())/100)
-            tempSamples.append(temp)
-        except Exception as e:
-            logger.error(e)
-
-        getUrlData = urlHue + sensor + sLastTime
-        sensorTime = subprocess.Popen(getUrlData, shell=True, stdout=subprocess.PIPE)
-        sTime = sensorTime.stdout.read().decode("utf-8").replace('"\n','').replace('"','')
-
-        now = datetime.now() 
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-       
-        delta = now - datetime.strptime(sTime, "%Y-%m-%dT%H:%M:%S") - timedelta(hours=1)
-        sensorTimeSamples.append(str(delta).split('.')[0])
-
-        getUrlData = urlHue + sensor + sBatt
-        battery = subprocess.Popen(getUrlData, shell=True, stdout=subprocess.PIPE)
-        batt = str(float(battery.stdout.read()))
-        battSamples.append(batt)
+def retreive_data():
     
+    data = []
+
+    presence = []
+    pres_sens_no = []
+    pres_batteries = []
+    pres_names = []
+
+    temperatures = []
+    temp_sens_no = []
+    temp_batteries = []
+
+    lights = []
+    light_sens_no = []
+    light_batteries = []
+
+    uid = []
+    uids = []
+ 
+
+    response = requests.get("http://192.168.50.151/api/HTymPjBT0g1JdTwXFdYe-N26G9IQ8MDQ8quVIkr1/")
+    response.raise_for_status()  # Check if the request was successful
+    data = response.json()  # Parse the JSON data
+
+    detailes = get_details_template(os.getcwd()+PATH + TEMPLATE_FILE)
+    for key, value in data.items():
+        if key == 'sensors':               
+            for sensor_id, sensor_data in value.items():
+                if sensor_data['type'] == 'ZLLTemperature':
+                    temp_sens_no.append(sensor_id)
+                    detail = get_detail_template(os.getcwd()+PATH + TEMPLATE_ITEM_FILE)
+                    detail = detail.replace("#DESCRIPTION#", sensor_data['type'])
+                    for sen_key, sen_value in sensor_data.items():
+                        if sen_key == 'state':
+                            detail = detail.replace("#DATE#", sen_value['lastupdated'])
+                            for val_key, val in sen_value.items():
+                                if val_key == 'temperature':
+                                    temperatures.append(val)
+                                    detail = detail.replace("#DATA#", str(val))
+                        if sen_key == 'config':
+                            temp_batteries.append(sen_value['battery'])
+                            detail = detail.replace("#BATT#", str(sen_value['battery']))
+                        if sen_key == 'uniqueid':
+                            uid.append(sen_value)
+                            detail = detail.replace("#ID#", str(sen_value))
+                    detailes = detailes.replace("</Devices-Detail-Response>", detail)
+                    detailes = detailes + "\n</Devices-Detail-Response>"
+                    detail = ''
+
+                if sensor_data['type'] == 'ZLLLightLevel':
+                    light_sens_no.append(sensor_id)
+                    detail = get_detail_template(os.getcwd()+PATH + TEMPLATE_ITEM_FILE)
+                    detail = detail.replace("#DESCRIPTION#", sensor_data['type'])
+                    for sen_key, sen_value in sensor_data.items():
+                        if sen_key == 'state':
+                            detail = detail.replace("#DATE#", sen_value['lastupdated'])
+                            for val_key, val in sen_value.items():
+                                if val_key == 'lightlevel':
+                                    lights.append(val)
+                                    detail = detail.replace("#DATA#", str(val))
+                        if sen_key == 'config':
+                            light_batteries.append(sen_value['battery'])
+                            detail = detail.replace("#BATT#", str(sen_value['battery']))
+                        if sen_key == 'uniqueid':
+                            uid.append(sen_value)
+                            detail = detail.replace("#ID#", str(sen_value))
+
+                    detailes = detailes.replace("</Devices-Detail-Response>", detail)
+                    detailes = detailes + "\n</Devices-Detail-Response>"
+                    detail = ''
+
+                if sensor_data['type'] == 'ZLLPresence':
+                    pres_sens_no.append(sensor_id)
+                    detail = get_detail_template(os.getcwd()+PATH + TEMPLATE_ITEM_FILE)
+                    detail = detail.replace("#DESCRIPTION#", sensor_data['type'])
+                    for sen_key, sen_value in sensor_data.items():
+                        if sen_key == 'state':
+                            detail = detail.replace("#DATE#", sen_value['lastupdated'])
+                            for val_key, val in sen_value.items():
+                                if val_key == 'presence':
+                                    presence.append(val)
+                                    detail = detail.replace("#DATA#", str(val))
+                        if sen_key == 'config':
+                            pres_batteries.append(sen_value['battery'])
+                            detail = detail.replace("#BATT#", str(sen_value['battery']))
+                        if sen_key == 'name':
+                            pres_names.append(sen_value )
+                            detail = detail.replace("#NAME#", str(sen_value))
+                        if sen_key == 'uniqueid':
+                            uid.append(sen_value)
+                            detail = detail.replace("#ID#", str(sen_value))
+                    
+                    detailes = detailes.replace("</Devices-Detail-Response>", detail)
+                    detailes = detailes + "\n</Devices-Detail-Response>"
+                    detail = ''
+
+                if len(uid) == 3:
+                    uids.append(uid)
+                    uid = []
+
+    # Print temperature and battery table
+    temp_table = zip(temp_sens_no, [float(temp)/100 for temp in temperatures], lights, presence, temp_batteries, pres_names)
+    # Clear the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(tabulate(temp_table, headers=["ID", "Temp (C)", "Light", "Presence" ,"Battery (%)", "Name"], tablefmt="fancy_grid"))
     
+
+def main():
+    poll_count = 0
+    error_count = 0
+    success_count = 0
+    retreive_data()
+    create_detail(os.getcwd()+PATH + TEMPLATE_ITEM_FILE)
+    
+def create_detail(template_file):
+    try:
+        with open(template_file, 'r') as file:
+            template = file.read()
+            file.close()
+    except Exception as e:
+        print("Current Directory:", os.getcwd())
+        logger.error(f"Error reading template file: {e}")
+        return
+
+def get_detail_template(template_file):
+    try:
+        with open(template_file, 'r') as file:
+            template = file.read()
+            file.close()
+    except Exception as e:
+        print("Current Directory:", os.getcwd())
+        logger.error(f"Error reading template file: {e}")
+    return template
+
+def get_details_template(template_file):
+    try:
+        with open(template_file, 'r') as file:
+            template = file.read()
+            file.close()
+    except Exception as e:
+        print("Current Directory:", os.getcwd())
+        logger.error(f"Error reading template file: {e}")
+    return template
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        logger.info("Process interrupted by user.")
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
