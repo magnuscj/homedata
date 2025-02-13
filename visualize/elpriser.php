3c92792824cd04adb82d3c4b2f0442ad7fc23b31
magnuscj <magnus.c.johansson@gmail.com>
2025-02-13 16:54:01 +0100

    Experiment

--- visualize/elpriser.php
+++ visualize/elpriser.php
@@ -25,11 +25,12 @@ $prices = [];
 $fdate          = date("Y-m-d", mktime(0,0,0,date("m"),date("d")-1,date("Y")));
 $tdate          = date("Y-m-d", mktime(0,0,0,date("m"),date("d"),date("Y")));
 $ttime = $ftime = date('H:i',time());
+$ttimep = $ftimep = date('H',time())-1;
 $username       = getConfig("DBUSN");
 $password       = getConfig('DBPSW');
 $database       = getConfig('DBNAME');
 $serverHostName = getConfig('DBIP');
-$prices = getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime, "electricityprice", $serverHostName);
+$prices = getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttimep, "electricityprice", $serverHostName);
 
 $dta = array();
 $i=0;
@@ -74,9 +75,11 @@ foreach ($prices[0] as $price) {
 
 $bplot->SetFillColor($barcolors);
 $bplot->SetWidth(1);
+$bplot->SetColor('black');
 $graph->Add($bplot);
 
 $retXY = deltaChange(addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime,'11502682451740542577', $serverHostName)));
+$heater = deltaChange(addMissingTime(getDataFromDb($username, $password, $database, $fdate." ".$ftime, $tdate." ".$ttime,'10871475366841829943', $serverHostName)));
 
 $H_tot = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 $i=0;
@@ -84,27 +87,61 @@ $j=0;
 $hour1 = $hour2 = (int)(date('H', $retXY[1][$i]));
 
 foreach($retXY[0] as $x_valX) {
-        
-        if ($hour1 == $hour2) {
-                $H_tot[$j] = $H_tot[$j] + $x_valX/20000;
+        if($j<24) {
+                if ($hour1 == $hour2) {
+                        $E_tot[$j] = $E_tot[$j] + $x_valX/25000;
+                }
+                else {
+                        $hour1 = $hour2;
+                        $j++;
+                }
+                $i++;
+                if ($i < count($retXY[1])) {
+                        $hour2 = (int)(date('H', $retXY[1][$i]));
+                }
         }
-        else {
-                $hour1 = $hour2;
-                $j++;
-        }
-        $i++;
-        $hour2 = (int)(date('H', $retXY[1][$i]));
 }
-print_r($H_tot);
 
+$i=0;
+$j=0;
+$hour1 = $hour2 = (int)(date('H', $heater[1][$i]));
+foreach($heater[0] as $x_valX) {
+        if($j<24) {
+                if ($hour1 == $hour2) {
+                        $H_tot[$j] = $H_tot[$j] + $x_valX/25000;
+                }
+                else {
+                        $hour1 = $hour2;
+                        $j++;
+                }
+                $i++;
+                if ($i < count($retXY[1])) {
+                        $hour2 = (int)(date('H', $retXY[1][$i]));
+                }
+        }
+}
 
-$bplot2 = new BarPlot($H_tot);
-$bplot2->SetFillColor('azure4');
-$bplot2->SetWidth(1);
-$bplot2->SetColor('gray3');
-$bplot2->SetWeight(2);
+for ($x= 0 ; $x < count($H_tot) ;$x++) {
+        $E_tot[$x] = $E_tot[$x] - $H_tot[$x];
+}
+
+$lineplot1 = new BarPlot($H_tot);
+$lineplot1->SetFillColor('gray3');
+$lineplot1->SetWidth(1);
+$lineplot1->SetColor('black');
+$lineplot1->SetWeight(2);
+
+$lineplot2 = new BarPlot($E_tot);
+$lineplot2->SetFillColor('gray6');
+$lineplot2->SetWidth(1);
+$lineplot2->SetColor('black');
+$lineplot2->SetWeight(2);
+//$lineploit->SetFillGradient('aquamarine1','aquamarine1:0.2');
+// Create the grouped bar plot
+$gbplot = new AccBarPlot(array($lineplot1,$lineplot2));
+$gbplot->SetWidth(0.8);
+$graph->Add($gbplot);
 
 $gdImgHandler = $graph->Stroke(_IMG_HANDLER);
 $graph->img->Stream("/var/www/html/picture/".$fileName.".png");
-   
 ?>
