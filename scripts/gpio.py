aa7f06006808b48b3ad0fc437b971b19f708b8a4
Magnus <magnus.c.johansson@gmail.com>
2026-06-28 13:33:12 +0200

    Improve gpio.py robustness: key-based dict access, valve safety, timeouts, max watering cap

--- scripts/gpio.py
+++ scripts/gpio.py
@@ -1,11 +1,8 @@
-#!/usr/bin/pythoin3
+#!/usr/bin/python3
 import RPi.GPIO as GPIO
 import time
-import datetime
 from urllib.request import urlopen
-from itertools import islice
 import json
-import array
 import logging
 import signal
 import sys
@@ -35,19 +32,17 @@ potWet  = [45,45,45,45,45,45,45,45]
 potDry  = [35,35,35,35,35,35,35,35]
 potAct  = [1,1,1,1,1,1,1,1]
 watDur  = [180,180,180,180,180,180,180,180]
-potNames= ["name1","name2","name3","name4","name5","name6","name7","name8",]
+potNames= ["name1","name2","name3","name4","name5","name6","name7","name8"]
 hyst    = [0,0,0,0,0,0,0,0]
 soilHumidity = []
-dry = 35
-wet = 45
 url = "http://ws-gateway/get_livedata_info?"
-measure = 1
 NAME = 1
 WET = 2
 DRY = 3
 ACTIVE = 4
 DURATION = 5
 
+MAX_WATERING_PER_CYCLE = 600  # seconds, safety cap per pot
 
 logger.debug("Init done")
 
@@ -60,20 +55,20 @@ def readConfig():
     config = []
     try:
         logger.debug("Read configuration file")
-        configFile = open("config.json", "r")
-        config_json = json.loads(configFile.read())
-        config  = config_json['potConfig']
+        with open("config.json", "r") as configFile:
+            config_json = json.loads(configFile.read())
+            config = config_json['potConfig']
     except Exception as e:
         logger.error(e)
-        logger.error("Configuration could not be red")
+        logger.error("Configuration could not be read")
     return config
 
 def getConfig(item, cnf):
     configuration = []
     try:
         for c in cnf:
-            pv = c.values()
-            configuration.append(int(list(islice(pv,0,6))[item]))
+            pv = list(c.values())
+            configuration.append(int(pv[item]))
     except Exception as e:
         logger.error(e)
     logger.debug(configuration)
@@ -84,18 +79,21 @@ def measureHumidity(url):
     data = []
     logger.debug("Collect soil moisture measurements.")
     try:
-        response = urlopen(url,timeout=10)
+        response = urlopen(url, timeout=10)
         data_json = json.loads(response.read())
-        data  = data_json['ch_soil']
-    except  Exception as e:
+        data = data_json['ch_soil']
+    except Exception as e:
         logger.error("Couldn't reach soil measurement server.")
         logger.error(e)
-    
-    #Check the humidity
+        return soilHumidity
+
     for x in data:
-        values = x.values()
-        humVal = list(islice(values,0,4))[3].replace('%','')
-        soilHumidity.append(humVal)
+        try:
+            humVal = x['humidity'].replace('%', '')
+            soilHumidity.append(humVal)
+        except (KeyError, AttributeError) as e:
+            logger.error("Unexpected soil data format: {}".format(e))
+            soilHumidity.append("--")
     return soilHumidity
 
 def getPotNames(url):
@@ -103,17 +101,20 @@ def getPotNames(url):
     data = []
     logger.debug("Collect soil sensor names.")
     try:
-        response = urlopen(url)
+        response = urlopen(url, timeout=10)
         data_json = json.loads(response.read())
-        data  = data_json['ch_soil']
-    except  Exception as e:
+        data = data_json['ch_soil']
+    except Exception as e:
         logger.error("Couldn't reach soil measurement server.")
         logger.error(e)
-    
-    #Check the humidity
+        return potNames
+
     for x in data:
-        values = x.values()
-        potNames.append(list(islice(values,0,4))[1])
+        try:
+            potNames.append(x.get('name', ''))
+        except Exception as e:
+            logger.error("Unexpected name format: {}".format(e))
+            potNames.append('')
     return potNames
 
 def setupBoard():
@@ -126,19 +127,19 @@ def setupBoard():
     logger.info("Test watering mechanics")
     for b in range(8):
         GPIO.output(potPin[b], GPIO.HIGH)
-        time.sleep(100/1000)
+        time.sleep(0.1)
         time.sleep(5)
         GPIO.output(potPin[b], GPIO.LOW)
 
     for b in range(8):
         GPIO.output(potPin[b], GPIO.LOW)
-        time.sleep(100/1000)
+        time.sleep(0.1)
 
 signal.signal(signal.SIGINT, signal_handler)
 setupBoard()
 
 logger.info("Starting")
-while(1):
+while True:
     config = readConfig()
 
     try:
@@ -154,49 +155,58 @@ while(1):
         potAct  = [1,1,1,1,1,1,1,1]
         watDur  = [60,60,60,60,60,60,60,60]
 
-    potNames  = getPotNames(url)
+    potNames = getPotNames(url)
 
     logger.info("Time for measure")
     soilHumidity = measureHumidity(url)
+
+    if not soilHumidity:
+        logger.warning("No humidity data — skipping watering cycle")
+        time.sleep(60)
+        continue
 
     logger.debug("Pot name:      {}".format(' '.join(map(str, potNames))))
     logger.debug("Pot humidity:  {}".format('  '.join(map(str, soilHumidity))))
     logger.debug("Pot dry level: {}".format('  '.join(map(str, potDry))))
     logger.debug("Pot wet level: {}".format('  '.join(map(str, potWet))))
-    if len(soilHumidity) > 8:
-        noOfPots = 8
-    else:
-        noOfPots = len(soilHumidity)
-    
-    #Act on humidity
-    try:
-        if (1 or measure):
-            wateringCycle=1440
-            measure = 0
-            logger.info("Time for watering")
-            for b in range(noOfPots):
-                if(str(soilHumidity[b]) == "--"):
-                    soilHumidity[b] = 99
-                if(potAct[b]):
-                    if(int(soilHumidity[b]) <= potDry[b] or hyst[b]==1):
-                        GPIO.output(potPin[b], GPIO.HIGH)
-                        hyst[b]=1
-                        logger.info("Watering pot: " + potNo[b] + " (" + potNames[b] + " - Humidity: " + str(soilHumidity[b]) + "(" + str(potDry[b]) + "/" + str(potWet[b]) + ")")
-                        time.sleep(watDur[b])
-                        wateringCycle=wateringCycle-watDur[b]
-                        GPIO.output(potPin[b], GPIO.LOW)
-                        time.sleep(1/3)
-                    if(int(soilHumidity[b]) >= potWet[b] and int(soilHumidity[b]) != 99):
-                        hyst[b]=0
-                        GPIO.output(potPin[b], GPIO.LOW)
-                        logger.info("The pot is too wet: " + potNo[b] + " ("+potNames[b]+" - Humidity: " + str(soilHumidity[b]) + "(" + str(potDry[b]) + "/" + str(potWet[b]) + ")")
-            logger.info("Watering done")
-            logger.info("Let's sleep for " + str(wateringCycle) + " seconds.")
-            time.sleep(wateringCycle)
-    except Exception as e:
-        logger.error("Couldn't act on humidity properly")
-        logger.error(e)
-        logger.error("Number of pots" + str(noOfPots))
-        time.sleep(60)
 
-GPIO.cleanup()
+    noOfPots = min(len(soilHumidity), 8)
+
+    wateringCycle = 1440
+    logger.info("Time for watering")
+    for b in range(noOfPots):
+        if str(soilHumidity[b]) == "--":
+            soilHumidity[b] = 99
+        if not potAct[b]:
+            continue
+        try:
+            humidity = int(soilHumidity[b])
+        except ValueError:
+            logger.error("Invalid humidity value for pot {}: {}".format(potNo[b], soilHumidity[b]))
+            continue
+
+        if humidity <= potDry[b] or hyst[b] == 1:
+            duration = min(watDur[b], MAX_WATERING_PER_CYCLE)
+            try:
+                GPIO.output(potPin[b], GPIO.HIGH)
+                hyst[b] = 1
+                logger.info("Watering pot: {} ({} - Humidity: {}({}/{}))".format(
+                    potNo[b], potNames[b] if b < len(potNames) else "?",
+                    humidity, potDry[b], potWet[b]))
+                time.sleep(duration)
+                wateringCycle -= duration
+            finally:
+                GPIO.output(potPin[b], GPIO.LOW)
+            time.sleep(1/3)
+
+        if humidity >= potWet[b] and humidity != 99:
+            hyst[b] = 0
+            GPIO.output(potPin[b], GPIO.LOW)
+            logger.info("The pot is too wet: {} ({} - Humidity: {}({}/{}))".format(
+                potNo[b], potNames[b] if b < len(potNames) else "?",
+                humidity, potDry[b], potWet[b]))
+
+    logger.info("Watering done")
+    sleepTime = max(wateringCycle, 60)
+    logger.info("Let's sleep for {} seconds.".format(sleepTime))
+    time.sleep(sleepTime)
